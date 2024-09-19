5daeb688d1a2f0eec95c295780074d1c8c00fffc
Nova / Kays <165211324+KaysiRB@users.noreply.github.com>
2024-09-19 16:20:47 +0200

    Update multitool.py

--- multitool.py
+++ multitool.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from colorama import Fore, Style, init
 from Tools import system_info, file_manager, network_tools
 from utils import common
@@ -20,11 +21,12 @@ logo = f"""
 
 hub = f"""
 {Fore.CYAN}                                      ╔══════════════════════════════════════╗
-{Fore.CYAN}                                      ║              {Fore.LIGHTGREEN_EX}Menu Principal{Fore.CYAN}          ║
+{Fore.CYAN}                                      ║ {Fore.MAGENTA}{common.version_tool}        {Fore.LIGHTGREEN_EX}Menu Principal{Fore.CYAN}        ║
 {Fore.CYAN}                                      ╠══════════════════════════════════════╣
 {Fore.CYAN}                                      ║ {Fore.YELLOW}1{Fore.CYAN} │ {Fore.LIGHTWHITE_EX}Informations système{Fore.CYAN}             ║
 {Fore.CYAN}                                      ║ {Fore.YELLOW}2{Fore.CYAN} │ {Fore.LIGHTWHITE_EX}Gestionnaire de fichiers{Fore.CYAN}         ║
 {Fore.CYAN}                                      ║ {Fore.YELLOW}3{Fore.CYAN} │ {Fore.LIGHTWHITE_EX}Outils réseau{Fore.CYAN}                    ║
+{Fore.CYAN}                                      ║ {Fore.YELLOW}4{Fore.CYAN} │ {Fore.LIGHTWHITE_EX}DDoS         {Fore.CYAN}                    ║
 {Fore.CYAN}                                      ╠══════════════════════════════════════╣
 {Fore.CYAN}                                      ║ {Fore.YELLOW}0{Fore.CYAN} │ {Fore.LIGHTRED_EX}Quitter{Fore.CYAN}                          ║
 {Fore.CYAN}                                      ╚══════════════════════════════════════╝
@@ -35,6 +37,7 @@ hub = f"""
 def main():
     import os
     import sys
+
 
     # Détecter le répertoire du script
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -49,7 +52,6 @@ def main():
         common.clear_screen()
         print(logo)
         print(hub)
-        print(common.version_tool)
         choice = common.get_user_input("Choix : ")
 
         if choice == '1':
@@ -58,6 +60,15 @@ def main():
             file_manager.manage_files()
         elif choice == '3':
             network_tools.show_network_menu()
+        elif choice == '4':
+            hulk_gui_path = os.path.join(script_dir, 'Tools', 'hulk_gui.py')
+            if os.path.exists(hulk_gui_path):
+                try:
+                    subprocess.run(['python', hulk_gui_path])  # Exécute hulk_gui.py
+                except Exception as e:
+                    common.print_error(f"Erreur lors de l'exécution du script : {e}")
+            else:
+                common.print_error("Le fichier hulk_gui.py est introuvable.")
         elif choice == '0':
             break
         else:
